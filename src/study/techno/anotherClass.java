9d524e1f2dc4e2e1c71219ea72b112bb1a55a63d
sinanyorulmaz <yorulmazsinan@gmail.com>
2020-12-21 14:59:19 +0200

    this is the first try

---
+++ src/study/techno/anotherClass.java
@@ -0,0 +1,7 @@
+package study.techno;
+
+public class anotherClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/study/techno/main.java
@@ -0,0 +1,7 @@
+package study.techno;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
